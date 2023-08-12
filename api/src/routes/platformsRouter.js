74545c511923ea7d63c2e2023fbb36502d402e24
KevinMCJ <kevin72309@gmail.com>
2023-08-12 12:13:54 -0300

    Only front in repository

--- api/src/routes/platformsRouter.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const platformsRouter = Router();
-const { getPlatformsHandler } = require("../handlers/platformsHandler");
-
-platformsRouter.get("/", getPlatformsHandler);
-
-module.exports = platformsRouter;
