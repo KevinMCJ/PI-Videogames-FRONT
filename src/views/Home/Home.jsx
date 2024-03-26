2924470619ffcfa118ef0b2612fe2a53cf68211f
Kevin Mamani <kevin72309@gmail.com>
2024-03-26 18:58:46 -0300

    fix: don't show previous pagination on new search

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   CardsContainer,
   SearchBar,
   Pagination,
   FilterSortSection,
   EditGame,
-} from "../../components";
-import { useSelector } from "react-redux";
-import styles from "./Home.module.css";
+} from '../../components';
+import { useSelector } from 'react-redux';
+import styles from './Home.module.css';
 
 const Home = () => {
   const videogames = useSelector((state) => state.app.copyGames);
   const isEditing = useSelector((state) => state.utils.isEditing);
+  const isLoading = useSelector((state) => state.app.isLoading);
   const [gamesToDisplay, setGamesToDisplay] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const gamesPerPage = 15;
@@ -35,7 +36,7 @@ const Home = () => {
       <FilterSortSection handlePageChange={handlePageChange} />
       <CardsContainer videogames={gamesToDisplay} />
       {/* // * Si la cantidad de juegos mostrados no es suficiente para completar una página, el paginado no se renderiza.*/}
-      {videogames.length > gamesPerPage && (
+      {videogames.length > gamesPerPage && !isLoading && (
         <Pagination
           currentPage={currentPage}
           totalPages={totalPages}
